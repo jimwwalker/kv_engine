e19c46c7f4b5bcaff0fb9fbbae73753c5c42ad32
Jim Walker <jim@couchbase.com>
2018-01-30 20:22:40 +0000

    dont run gtests
    Change-Id: Ibea366b92661fed9cf93b9c303af2e30061cf29f

--- engines/ep/benchmarks/ep_engine_benchmarks_main.cc
+++ engines/ep/benchmarks/ep_engine_benchmarks_main.cc
@@ -39,5 +39,5 @@ int main(int argc, char** argv) {
     ::benchmark::Initialize(&argc, argv);
     ::benchmark::RunSpecifiedBenchmarks();
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    return 0;//RUN_ALL_TESTS();
 }
