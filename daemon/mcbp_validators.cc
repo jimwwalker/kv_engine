f37cf0e9fdc634bc94f045ac5c49f0a6de9d97be
Jim Walker <jim@couchbase.com>
2020-06-15 21:40:05 +0100

    MB-39594: Only call get_scope_id if we need to
    Currently all incoming operations result in a get_scope_id
    call so that scope/collection privileges can be checked.
    
    However if the connection only has bucket privileges defined
    this lookup (and cost per operation) is not required.
    
    Change-Id: I5488d1460cdcb02a3e066432ff652a0d237b335d

--- daemon/mcbp_validators.cc
+++ daemon/mcbp_validators.cc
@@ -364,7 +364,8 @@ Status McbpValidator::verify_header(Cookie& cookie,
 
     if (connection.getBucket().type != BucketType::NoBucket &&
         cb::mcbp::is_collection_command(request.getClientOpcode()) &&
-        connection.getBucket().supports(cb::engine::Feature::Collections)) {
+        connection.getBucket().supports(cb::engine::Feature::Collections) &&
+        connection.getPrivilegeContext().hasScopePrivileges()) {
         // verify that we can map the connection to sid. To make our
         // unit tests easier lets go through the connection
         auto key = cookie.getRequestKey();
