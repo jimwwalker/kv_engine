29193b2e027169b2cc2bc2a0de44b0ad516c5cdd
Jim Walker <jim@couchbase.com>
2020-09-23 15:42:27 +0100

    MB-39292: 2/n Change from JSON to flatbuffers+crc32c
    Change from a JSON format to a flatbuffers defined structure.
    Reload of the manifest can use the flatbuffers Verify which
    checks that the loaded data appears to be a valid 'Manifest'.
    
    A CRC (using crc32c) is further added to the stored data so
    that we can detect changes to the data which may not be
    detected by Verify (e.g. a string value "dog" changing to "cat").
    
    Detected issues are logged with CRITICAL - the bucket is allowed to
    continue warm-up and the in-memory Bucket::Manifest is initialised as
    the 'epoch' Manifest, the bucket will then pick-up the current
    manifest from ns_server, any Manifest is a successor of epoch.
    
    Change-Id: I319c3e76ef5c07da0680e602f3e342d3d8affa58

--- engines/ep/src/collections/manager.cc
+++ engines/ep/src/collections/manager.cc
@@ -315,14 +315,23 @@ void Collections::Manager::addScopeStats(KVBucket& bucket,
 
 void Collections::Manager::warmupLoadManifest(const std::string& dbpath) {
     auto rv = Collections::PersistManifestTask::tryAndLoad(dbpath);
-    if (rv) {
-        EP_LOG_INFO(
-                "Collections::Manager::warmupLoadManifest: loaded uid:{:#x}",
-                rv->getUid());
-        *currentManifest.wlock() = std::move(*rv);
-    } else {
-        EP_LOG_INFO("Collections::Manager::warmupLoadManifest: nothing loaded");
+    if (rv.has_value()) {
+        if (rv.value()) {
+            EP_LOG_INFO(
+                    "Collections::Manager::warmupLoadManifest: loaded "
+                    "uid:{:#x}",
+                    rv.value()->getUid());
+            *currentManifest.wlock() = std::move(*rv.value());
+        } else {
+            EP_LOG_INFO(
+                    "Collections::Manager::warmupLoadManifest: nothing loaded");
+        }
     }
+    // else tryAndLoad detected (and logged) some kind of corruption issue.
+    // If this corruption occurred at the same time as some issue in the
+    // forward flow of the Manifest, KV can't validate that any change to the
+    // manifest is a legal successor (Manifest::isSuccessor) - we will just have
+    // to accept what we're given and carry on.
 }
 
 /**
