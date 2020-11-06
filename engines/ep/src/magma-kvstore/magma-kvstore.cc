f16744ce98c467cf12810cf9d9429a28a2addaf9
Jim Walker <jim@couchbase.com>
2020-11-06 20:08:00 +0000

    MB-41882: Remove purgeSeq from CompactionConfig
    CompactionConfig::purgeSeq is never used, we can also
    tidy up the CompactTask - just read the purgeSeqno from
    the vbucket before running compaction.
    
    Change-Id: Idebd0d7f93e99968bfbf53de8459fd75effc39c0

--- engines/ep/src/magma-kvstore/magma-kvstore.cc
+++ engines/ep/src/magma-kvstore/magma-kvstore.cc
@@ -1881,13 +1881,12 @@ bool MagmaKVStore::compactDBInternal(std::shared_ptr<CompactionContext> ctx) {
 
     logger->info(
             "MagmaKVStore::compactDBInternal: {} purge_before_ts:{} "
-            "purge_before_seq:{} drop_deletes:{} purgeSeq:{} "
+            "purge_before_seq:{} drop_deletes:{} "
             "retain_erroneous_tombstones:{}",
             ctx->vbid,
             ctx->compactConfig.purge_before_ts,
             ctx->compactConfig.purge_before_seq,
             ctx->compactConfig.drop_deletes,
-            ctx->compactConfig.purgeSeq,
             ctx->compactConfig.retain_erroneous_tombstones);
 
     Vbid vbid = ctx->vbid;
@@ -2356,12 +2355,11 @@ std::shared_ptr<CompactionContext> MagmaKVStore::makeCompactionContext(
     logger->info(
             "MagmaKVStore::makeCompactionContext {} purge_before_ts:{} "
             "purge_before_seq:{}"
-            " drop_deletes:{} purgeSeq:{} retain_erroneous_tombstones:{}",
+            " drop_deletes:{} retain_erroneous_tombstones:{}",
             ctx->vbid,
             ctx->compactConfig.purge_before_ts,
             ctx->compactConfig.purge_before_seq,
             ctx->compactConfig.drop_deletes,
-            ctx->compactConfig.purgeSeq,
             ctx->compactConfig.retain_erroneous_tombstones);
 
     return ctx;
