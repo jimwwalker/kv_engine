fffdf5e619ecdd683420bd29fe24e0b7fe3441f9
Jim Walker <jim@couchbase.com>
2020-11-24 15:46:58 +0000

    MB-42885: Post rollback set the 'on-disk' item count directly from KVStore
    During rollback the callback class maintains the on-disk total by
    comparing pre/post item state and invoking increments and decrements.
    
    This can be removed and simplified by just (re)setting the on-disk
    total from the rolled back KVStore.
    
    Change-Id: I089e2a80ee504b0707212eb1c2d62f109375d559

--- engines/ep/src/ep_bucket.cc
+++ engines/ep/src/ep_bucket.cc
@@ -1592,18 +1592,6 @@ public:
                        << " after setFromInternal for item:" << *preRbSeqnoItem;
                     throw std::logic_error(ss.str());
                 }
-
-                // If we are rolling back a deletion then we should increment
-                // our disk counts. We need to increment the vBucket disk
-                // count here too because we're not going to flush this item
-                // later
-                if (postRbSeqnoItem->isDeleted() &&
-                    postRbSeqnoItem->isCommitted()) {
-                    vb->incrNumTotalItems();
-                    vb->getManifest()
-                            .lock(preRbSeqnoItem->getKey())
-                            .incrementItemCount();
-                }
             }
         } else if (preRbSeqnoGetValue.getStatus() == ENGINE_KEY_ENOENT) {
             EP_LOG_DEBUG(
@@ -1627,12 +1615,6 @@ public:
             // Document didn't exist in memory - may have been deleted in since
             // the checkpoint.
             setStatus(ENGINE_KEY_ENOENT);
-        }
-
-        if (!item.isDeleted() && item.isCommitted()) {
-            // Irrespective of if the in-memory delete succeeded; the document
-            // doesn't exist on disk; so decrement the item count.
-            vb.decrNumTotalItems();
         }
     }
 
