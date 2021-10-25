e93e5a4fc540647c873ced81327a0c4476d51d2e
Jim Walker <jim@couchbase.com>
2021-10-25 10:27:07 +0100

    MB-49040: 2/n Add DataLimit to VB::Manifest scopes
    Add to the VB:Manifest scope metadata a DataLimit and then copy the
    Manifest dataLimit through to the VB::Manifest when scopes are created.
    
    Change-Id: Id41cdce14e8ca50b3c6c9b98471e5c655fce5a67

--- engines/ep/src/collections/vbucket_manifest_scope_entry.h
+++ engines/ep/src/collections/vbucket_manifest_scope_entry.h
@@ -63,6 +63,11 @@ public:
         return sharedMeta->name;
     }
 
+    /// @return the optional data limit
+    DataLimit getDataLimit() const {
+        return sharedMeta->dataLimit;
+    }
+
     /**
      * Take the ScopeSharedMetaData from this entry (moves the data) to
      * the caller.
