4d76c669cd400f7c311597ce62ac2e92188d2f3c
Jim Walker <jim@couchbase.com>
2021-12-17 11:50:01 +0000

    MB-35297: Remove complete from the DCPBackfill::cancel path
    DCPBackfill::cancel is invoked when shouldCancel returns true.
    This only happens if:
    
    * ActiveStream is dead
    * ActiveStream has gone (weak_ptr cannot lock)
    
    In either of those cases the complete function (implemented at the
    BySeqno/ByID) level of the backfill should not be called.
    
    1) If the ActiveStream has gone, complete will fail to lock the
       weak_ptr.
    2) If the stream can be obtained, calling completeBackfill (or OSO
       equivalent) is pointless on a dead stream, for example any
       DcpResponse queued is going no where.
    
    This commit aims to simplify based on the above description, removing
    the call to complete from the cancel path. This makes the
    implementation of complete much simpler as it doesn't need to consider
    logging "cancel".
    
    Change-Id: I01e786ada63888f46ac5eac01fd683b60d6f6717

--- engines/ep/src/dcp/backfill_disk.cc
+++ engines/ep/src/dcp/backfill_disk.cc
@@ -170,7 +170,7 @@ backfill_status_t DCPBackfillDisk::run() {
     case State::scan:
         return scan();
     case State::complete:
-        complete(false);
+        complete();
         return backfill_finished;
     case State::done:
         return backfill_finished;
@@ -184,7 +184,10 @@ backfill_status_t DCPBackfillDisk::run() {
 void DCPBackfillDisk::cancel() {
     std::lock_guard<std::mutex> lh(lock);
     if (state != State::done) {
-        complete(true);
+        EP_LOG_WARN(
+                "DCPBackfillDisk::cancel ({}) cancelled before reaching "
+                "State::done",
+                getVBucketId());
     }
 }
 
