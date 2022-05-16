6c19602e6be90b4024436a1358ea837f08332aa0
Jim Walker <jim@couchbase.com>
2022-05-16 16:44:55 +0100

    MB-35297: RangeScan continue using mcbp
    Add mcbp validator/executor so that an existing RangeScan can be
    continued.
    
    A RangeScan continue is a bit different to other mcbp commands.
    
    1) A single continue request may trigger many response packets, with
    each response carrying a payload containing many keys/values.
    See range_scan_continue.md for a description of the payload format.
    
    2) The RangeScan I/O task (AUXIO) is writing 'directly' to the
    connection. This is different to the usual ewould_block pattern
    as we don't bring data into memory and then trigger a worker thread
    to copy that memory out. Note that the continue request does not
    use out-of-order and the connection cannot process any more commands,
    even whilst waiting for the AUXIO task to pickup the continue task.
    
    Change-Id: I3dcd0c8445190bb1f313d2b2746779bff7ff3cbf

--- tests/testapp/testapp_range_scan.cc
+++ tests/testapp/testapp_range_scan.cc
@@ -13,8 +13,14 @@
 
 #include <protocol/connection/client_mcbp_commands.h>
 
+#include <xattr/blob.h>
+#include <xattr/utils.h>
+
+#include <mcbp/codec/range_scan_continue_codec.h>
 #include <memcached/range_scan_id.h>
 #include <platform/base64.h>
+
+#include <unordered_set>
 
 class RangeScanTest : public TestappXattrClientTest {
 public:
@@ -23,12 +29,62 @@ public:
             GTEST_SKIP();
         }
         TestappXattrClientTest::SetUp();
-        start = Couchbase::Base64::encode("user");
-        end = Couchbase::Base64::encode("user\xFF");
+        mInfo = storeTestKeys();
 
-        config = {{"range", {{"start", start}, {"end", end}}}};
+        start = cb::base64::encode("user", false);
+        end = cb::base64::encode("user\xFF", false);
+
+        // Setup to scan for user prefixed docs. Utilise wait_for_seqno
+        config = {{"range", {{"start", start}, {"end", end}}},
+                  {"snapshot_requirements",
+                   {{"seqno", mInfo.seqno},
+                    {"vb_uuid", mInfo.vbucketuuid},
+                    {"timeout_ms", 120000}}}};
+
+        // if snappy evict so values comes from disk and we can validate snappy
+        if (::testing::get<3>(GetParam()) == ClientSnappySupport::Yes) {
+            adminConnection->executeInBucket(
+                    bucketName, [this](auto& connection) {
+                        for (const auto& key : userKeys) {
+                            connection.evict(key, Vbid(0));
+                        }
+                    });
+        }
     }
 
+    // All successful scans are scanning for these keys
+    std::unordered_set<std::string> userKeys = {"user-alan",
+                                                "useralan",
+                                                "user.claire",
+                                                "user::zoe",
+                                                "user:aaaaaaaa",
+                                                "users"};
+    // Other keys that get stored in the bucket
+    std::vector<std::string> otherKeys = {
+            "useq", "uses", "abcd", "uuu", "uuuu", "xyz"};
+
+    MutationInfo storeTestKeys() {
+        for (const auto& key : userKeys) {
+            store_document(key, key);
+        }
+
+        for (const auto& key : otherKeys) {
+            store_document(key, key);
+        }
+
+        Document doc;
+        doc.value = "persist me";
+        doc.info.id = "final";
+        return userConnection->mutate(doc, Vbid(0), MutationType::Set);
+    }
+
+    void drainKeyResponse(const BinprotResponse& response);
+
+    void drainItemResponse(const BinprotResponse& response);
+
+    void drainScan(cb::rangescan::Id id, bool keyScan, size_t itemLimit);
+
+    MutationInfo mInfo;
     std::string start;
     std::string end;
     nlohmann::json config;
@@ -95,4 +151,109 @@ TEST_P(RangeScanTest, CreateCancel) {
     userConnection->sendCommand(cancel);
     userConnection->recvResponse(resp);
     ASSERT_EQ(cb::mcbp::Status::KeyEnoent, resp.getStatus());
+}
+
+void RangeScanTest::drainKeyResponse(const BinprotResponse& response) {
+    if (response.getDataView().empty()) {
+        return;
+    }
+    cb::mcbp::response::RangeScanContinueKeyPayload payload(
+            response.getDataView());
+
+    auto key = payload.next();
+    while (key.data()) {
+        EXPECT_EQ(1, userKeys.count(std::string{key})) << key;
+        key = payload.next();
+    }
+}
+
+void RangeScanTest::drainItemResponse(const BinprotResponse& response) {
+    if (response.getDataView().empty()) {
+        return;
+    }
+    cb::mcbp::response::RangeScanContinueValuePayload payload(
+            response.getDataView());
+
+    auto record = payload.next();
+    while (record.key.data()) {
+        EXPECT_EQ(1, userKeys.count(std::string{record.key}));
+
+        if (mcbp::datatype::is_snappy(record.meta.getDatatype())) {
+            cb::compression::Buffer buffer;
+            EXPECT_TRUE(cb::compression::inflate(
+                    cb::compression::Algorithm::Snappy, record.value, buffer));
+            EXPECT_EQ(1, userKeys.count(std::string{std::string_view{buffer}}))
+                    << record.key;
+        } else {
+            EXPECT_EQ(1, userKeys.count(std::string{record.value}));
+        }
+
+        record = payload.next();
+    }
+}
+
+void RangeScanTest::drainScan(cb::rangescan::Id id,
+                              bool keyScan,
+                              size_t itemLimit) {
+    BinprotResponse resp;
+
+    do {
+        // Keep sending continue until we get the response with complete
+        BinprotRangeScanContinue scanContinue(
+                Vbid(0), id, itemLimit, std::chrono::milliseconds(0));
+        userConnection->sendCommand(scanContinue);
+
+        // Keep receiving responses until the sequence ends (!success)
+        while (true) {
+            userConnection->recvResponse(resp);
+
+            // An error or more/complete status may carry keys/values, so try
+            // and drain the payload
+            if (keyScan) {
+                drainKeyResponse(resp);
+            } else {
+                drainItemResponse(resp);
+            }
+
+            if (resp.getStatus() != cb::mcbp::Status::Success) {
+                // Stop this loop once !success is seen
+                break;
+            }
+        }
+
+        // Don't expect any errors in this scan. It should return only the
+        // following status codes for each response.
+        ASSERT_TRUE(resp.getStatus() == cb::mcbp::Status::Success ||
+                    resp.getStatus() == cb::mcbp::Status::RangeScanMore ||
+                    resp.getStatus() == cb::mcbp::Status::RangeScanComplete)
+                << resp.getStatus();
+    } while (resp.getStatus() != cb::mcbp::Status::RangeScanComplete);
+}
+
+TEST_P(RangeScanTest, KeyOnly) {
+    config["key_only"] = true;
+
+    BinprotRangeScanCreate create(Vbid(0), config);
+    userConnection->sendCommand(create);
+
+    BinprotResponse resp;
+    userConnection->recvResponse(resp);
+    ASSERT_EQ(cb::mcbp::Status::Success, resp.getStatus());
+    cb::rangescan::Id id;
+    std::memcpy(id.data, resp.getData().data(), resp.getData().size());
+
+    drainScan(id, true, 2); // 2 items per continue
+}
+
+TEST_P(RangeScanTest, ValueScan) {
+    BinprotRangeScanCreate create(Vbid(0), config);
+    userConnection->sendCommand(create);
+
+    BinprotResponse resp;
+    userConnection->recvResponse(resp);
+    ASSERT_EQ(cb::mcbp::Status::Success, resp.getStatus());
+    cb::rangescan::Id id;
+    std::memcpy(id.data, resp.getData().data(), resp.getData().size());
+
+    drainScan(id, false, 2); // 2 items per continue
 }
